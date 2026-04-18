3631ecfe1a393676b8792dc84df1f12b7b2b4c1d
felixemmanuel8279-rgb <felixemmanuel8279@gmail.com>
2026-04-18 06:13:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,67 @@
-function addProject() {
-    let title = document.getElementById("title").value;
-    let desc = document.getElementById("desc").value;
+// FIREBASE IMPORT
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-    if (!title || !desc) {
-        alert("Fill all fields");
-        return;
-    }
+// CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyCiP50WPzxYhTO0guptT64PpWLWSnq0aVs",
+  authDomain: "felix-dev-aa428.firebaseapp.com",
+  projectId: "felix-dev-aa428",
+  storageBucket: "felix-dev-aa428.firebasestorage.app",
+  messagingSenderId: "206240559196",
+  appId: "1:206240559196:web:a2726c34e79f5f8beb7699"
+};
 
-    let container = document.getElementById("projects");
+// INIT
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// ADD PROJECT
+window.addProject = async function () {
+  let title = document.getElementById("title").value;
+  let desc = document.getElementById("desc").value;
+
+  if (!title || !desc) {
+    alert("Fill all fields");
+    return;
+  }
+
+  await addDoc(collection(db, "projects"), { title, desc });
+
+  alert("Saved!");
+  loadProjects();
+};
+
+// LOAD PROJECTS
+async function loadProjects() {
+  let container = document.getElementById("projects");
+  container.innerHTML = "";
+
+  let snapshot = await getDocs(collection(db, "projects"));
+
+  snapshot.forEach(doc => {
+    let data = doc.data();
 
     container.innerHTML += `
-        <div class="card">
-            <h3>${title}</h3>
-            <p>${desc}</p>
-        </div>
+      <div class="card">
+        <h3>${data.title}</h3>
+        <p>${data.desc}</p>
+      </div>
     `;
+  });
 }
 
-function openWhatsApp() {
-    window.open("https://wa.me/2347049138962", "_blank");
-}
+loadProjects();
 
-function makeCall() {
-    window.location.href = "tel:07049138962";
-}
+// CONTACT
+window.openWhatsApp = function () {
+  window.open("https://wa.me/2347049138962", "_blank");
+};
 
-function sendEmail() {
-    window.location.href = "mailto:felixemmanuel18279@gmail.com";
-}
+window.makeCall = function () {
+  window.location.href = "tel:07049138962";
+};
+
+window.sendEmail = function () {
+  window.location.href = "mailto:felixemmanuel8279@gmail.com";
+};
